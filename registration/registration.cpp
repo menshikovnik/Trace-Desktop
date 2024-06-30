198a350689f9eadbbc6999bd196363223f42e5ee
menshikovnik <nikmenshikov1@gmail.com>
2024-07-01 02:40:03 +0300

    add slot, remove autosave && .ui files

--- registration/registration.cpp
+++ registration/registration.cpp
@@ -5,6 +5,8 @@
 RegistrationWindow::RegistrationWindow(QWidget *parent)
     : QDialog(parent), ui(new Ui::RegistrationWindow) {
     ui->setupUi(this);
+
+    connect(ui->ok_button, &QPushButton::clicked, this, &RegistrationWindow::on_OK_button_clicked);
 }
 
 RegistrationWindow::~RegistrationWindow() {
@@ -12,14 +14,13 @@ RegistrationWindow::~RegistrationWindow() {
 }
 
 void RegistrationWindow::on_OK_button_clicked() {
-    QString username = ui->login_input_field->text(); // Получаем текст из поля ввода имени пользователя
-    QString password = ui->password_input_field->text(); // Получаем текст из поля ввода пароля
+    QString username = ui->login_input_field->text();
+    QString password = ui->password_input_field->text();
 
-    // Пример простой проверки
+
     if (username.isEmpty() || password.isEmpty()) {
         QMessageBox::warning(this, "Registration Error", "Please fill in all fields");
     } else {
-        // Здесь можно добавить код для обработки регистрации, например, отправку данных на сервер
         QMessageBox::information(this, "Registration Success", "You have been registered successfully!");
     }
 }
